f61b83d70da4824126400f9ad98df86e74366b7b
Leonardo Daniel Chavarria Zamora <leonardochavarria@MacBook-Pro-de-Leonardo.local>
2026-02-13 19:45:40 -0600

    first sentence removed

--- index.js
+++ index.js
@@ -33,7 +33,11 @@ app.post("/", async (req, res) => {
     try {
       const response = await axios.get(api_url + artist + "/" + title);
       const result = response.data.lyrics;
-      const debuggedLyrics = result.replace(/^.*(\r\n|\n)/, "");
+      const debuggedLyrics = result
+        .replace(/\\r\\n|\\n/g, "\n")
+        .split("\n")
+        .slice(1)
+        .join("\n");
       res.render("index.ejs", {
         data: debuggedLyrics,
         artist: artist,
